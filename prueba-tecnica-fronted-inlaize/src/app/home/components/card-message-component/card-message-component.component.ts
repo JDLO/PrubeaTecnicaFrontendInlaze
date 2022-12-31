cf242419df6ceedf02226552ef7662ec5ec0d4e7
Juan David Leon <juandavidleono@gmail.com>
2022-12-31 17:32:10 -0500

    Se hacen cambios en el componente card message

--- prueba-tecnica-fronted-inlaize/src/app/home/components/card-message-component/card-message-component.component.ts
+++ prueba-tecnica-fronted-inlaize/src/app/home/components/card-message-component/card-message-component.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from '@app/home/model/message';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-card-message-component',
   templateUrl: './card-message-component.component.html',
   styleUrls: ['./card-message-component.component.css']
 })
-export class CardMessageComponentComponent {
+export class CardMessageComponentComponent implements OnInit {
+  @Input()
+  public message! : Message;
 
+  constructor(){}
+  ngOnInit(): void {}
 }
