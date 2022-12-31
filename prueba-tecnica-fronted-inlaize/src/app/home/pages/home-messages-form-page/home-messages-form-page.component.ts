ea826728c8ab6939dc9db516494b7f96a12af31c
Juan David Leon <juandavidleono@gmail.com>
2022-12-31 18:06:18 -0500

    Se crea una estructura para el componente

--- prueba-tecnica-fronted-inlaize/src/app/home/pages/home-messages-form-page/home-messages-form-page.component.ts
+++ prueba-tecnica-fronted-inlaize/src/app/home/pages/home-messages-form-page/home-messages-form-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '@app/home/model/message';
 
 @Component({
   selector: 'app-home-messages-form-page',
   templateUrl: './home-messages-form-page.component.html',
   styleUrls: ['./home-messages-form-page.component.css']
 })
-export class HomeMessagesFormPageComponent {
+export class HomeMessagesFormPageComponent implements OnInit {
+  public messages!: Message[];
+
+  ngOnInit(): void {
+      this.messages = [new Message(null)]
+  }
+
+  public constructor(){
+  }
 
 }
