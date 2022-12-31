d403b3b7c3437e61d887736c8cc62d1feaf3aef5
Juan David Leon <juandavidleono@gmail.com>
2022-12-31 17:30:51 -0500

    Se agregan las rutas

--- prueba-tecnica-fronted-inlaize/src/app/app-routing.module.ts
+++ prueba-tecnica-fronted-inlaize/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeMessagesFormPageComponent } from './home/pages/home-messages-form-page/home-messages-form-page.component';
+import { HomeMessagesPageComponent } from './home/pages/home-messages-page/home-messages-page.component';
+import { MyMessagesPageComponent } from './home/pages/my-messages-page/my-messages-page.component';
+import { LoginFormPageComponent } from './login-logout/login-form-page/login-form-page.component';
+import { SigninFormPageComponent } from './login-logout/signin-form-page/signin-form-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeMessagesPageComponent },
+  { path: 'message', component:  HomeMessagesFormPageComponent},
+  { path: 'message/my', component:  MyMessagesPageComponent},
+  { path: 'login', component: LoginFormPageComponent },
+  { path: 'signin', component: SigninFormPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
