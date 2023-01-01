f63ec562cdddb1e7ee66eb43c6e18f7e7461b8ad
Juan David Leon <juandavidleono@gmail.com>
2022-12-31 21:25:52 -0500

    Se agregan routers

--- prueba-tecnica-fronted-inlaize/src/app/app-routing.module.ts
+++ prueba-tecnica-fronted-inlaize/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PermissionsGuard } from './home/guards/permissions.guard';
 import { HomeMessagesFormPageComponent } from './home/pages/home-messages-form-page/home-messages-form-page.component';
 import { HomeMessagesPageComponent } from './home/pages/home-messages-page/home-messages-page.component';
 import { MyMessagesPageComponent } from './home/pages/my-messages-page/my-messages-page.component';
@@ -8,9 +9,12 @@ import { SigninFormPageComponent } from './login-logout/signin-form-page/signin-
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeMessagesPageComponent },
-  { path: 'message', component:  HomeMessagesFormPageComponent},
-  { path: 'message/my', component:  MyMessagesPageComponent},
+  { path: 'home', component: HomeMessagesPageComponent, canActivate: [PermissionsGuard],
+    children: [
+      { path: 'message/my', component:  MyMessagesPageComponent},
+      { path: 'message', component:  HomeMessagesFormPageComponent},
+  ]
+  },
   { path: 'login', component: LoginFormPageComponent },
   { path: 'signin', component: SigninFormPageComponent }
 ];
